fb506b968d12af062ffa6adf81ebaf798d81cde5
Kumar-rishab3 <165635075+Kumar-rishab3@users.noreply.github.com>
2025-07-03 13:21:23 +0530

    Testing commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,40 +1,19 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
-@Table(name = "users")
+@Data
 @Entity
+@Table(name = "users")
 public class User {
     @Version
-    private Long version;
+    private Integer version;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
